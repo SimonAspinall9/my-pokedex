2527efd617345c8ddaffe4f8c4ada0f407f74ff7
Simon <simon.aspinall@sharpgaming.com>
2021-11-08 15:02:25 +0000

    use new theme colours

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,45 @@ import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PokemonPage from "components/Pokemon";
 import { useState } from "react";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
 
 const App = () => {
   const [useDreamWorld, setUseDreamWorld] = useState(false);
   const [searchText, setSearchText] = useState("");
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#7D78A3",
+      },
+    },
+  });
+
   return (
-    <Router>
-      <Header
-        onDreamWorldChange={() => {
-          setUseDreamWorld(!useDreamWorld);
-        }}
-        onSearch={(e) => {
-          setSearchText(e.target.value);
-        }}
-      />
-      <Switch>
-        <Route
-          path="/"
-          component={() => (
-            <PokemonPage
-              searchText={searchText}
-              useDreamWorld={useDreamWorld}
-            />
-          )}
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header
+          onDreamWorldChange={() => {
+            setUseDreamWorld(!useDreamWorld);
+          }}
+          onSearch={(e) => {
+            setSearchText(e.target.value);
+          }}
         />
-      </Switch>
-    </Router>
+        <Switch>
+          <Route
+            path="/"
+            component={() => (
+              <PokemonPage
+                searchText={searchText}
+                useDreamWorld={useDreamWorld}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
